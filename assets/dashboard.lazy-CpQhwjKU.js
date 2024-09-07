d7c2b558a547edd6d57943c7e5a588ae3ffdc3ee
RomanShevchuk0 <roman.shevchuk@dmorpheus.design>
2024-09-07 13:26:34 +0200

    Updates

---
+++ assets/dashboard.lazy-CpQhwjKU.js
@@ -0,0 +1,1 @@
+import{c as o,j as r}from"./index-N4XmVBFl.js";const t=()=>r.jsx("div",{children:"Dashboard"}),e=o("/dashboard")({component:t});export{e as Route};
---
+++ assets/products.lazy-cYrE6JKh.js
@@ -0,0 +1,1 @@
+import{c as t,j as o}from"./index-N4XmVBFl.js";const r=()=>o.jsx("div",{children:"Products"}),s=t("/products")({component:r});export{s as Route};
